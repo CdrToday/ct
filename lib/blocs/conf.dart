75b23d4975e9509c27fcadca7e5dd55d491cbf5f
clearloop <udtrokia@gmail.com>
2019-09-09 04:13:33 +0800

    update version

--- lib/blocs/conf.dart
+++ lib/blocs/conf.dart
@@ -1,5 +1,5 @@
 Map<String, dynamic> conf = {
-  'url': 'http://127.0.0.1:6060/api/v0',
-  'upload': 'http://127.0.0.1:7070/upload',
+  // 'url': 'http://127.0.0.1:6060/api/v0',
+  'url': 'http://api.cdr.today/api/v0',
   'image': 'http://pxddtegnl.bkt.clouddn.com/'
 };
