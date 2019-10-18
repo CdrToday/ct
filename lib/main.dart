6d83f79ed5be740419dff52211eff412ede26ee4
clearloop <udtrokia@gmail.com>
2019-10-18 23:35:03 +0800

    fix photo uploading

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 // blocs
 import 'package:cdr_today/blocs/main.dart';
@@ -100,7 +101,12 @@ class _AppState extends State<App> {
         onGenerateRoute: router,
         home: SplashPage(),
         debugShowCheckedModeBanner: false,
-        theme: CtThemeData.gen()
+        theme: CtThemeData.gen(),
+        localizationsDelegates: [
+          DefaultMaterialLocalizations.delegate,
+          DefaultCupertinoLocalizations.delegate,
+          DefaultWidgetsLocalizations.delegate,
+        ],
       )
     );
   }
